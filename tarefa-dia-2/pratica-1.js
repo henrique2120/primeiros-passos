96c9c1e6b79ca4744348264904fb7fc5e77183ea
henrique2120 <riky.souzadias@gmail.com>
2022-08-27 10:08:44 -0300

    dia-2

---
+++ tarefa-dia-2/pratica-1.js
@@ -0,0 +1,11 @@
+let nome = `henrique`
+let email =`houhou@gma9il.com`
+let profissao = `empresario`
+let idade = 18
+let lugar = `junda`
+
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(lugar)
